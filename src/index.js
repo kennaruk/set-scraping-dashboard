32455f90efb8ce91da3dd02a480526b6b65b7911
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-12-01 19:07:01 +0700

    samples added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import "./env";
 
 import { scrapingStockDataBySymbol } from "./controllers/set";
 import { insertDataToInflux } from "./controllers/influx";
+// import fs from "fs";
+// const Json2csvParser = require("json2csv").Parser;
+// const fields = ["date", "open", "high", "low", "close"];
 
 const scrapingSETBySymbol = async ({ symbol = "" } = {}) => {
 	if (!symbol) return;
 	const data = await scrapingStockDataBySymbol({ symbol });
+	// const json2csvParser = new Json2csvParser({ fields });
+	// const csv = json2csvParser.parse(data);
+	// fs.writeFile(
+	// 	`${__dirname}/${symbol}-${new Date().getTime()}.csv`,
+	// 	csv,
+	// 	"utf8",
+	// 	() => {
+	// 		console.log(`write ${symbol}`);
+	// 	}
+	// );
 	await insertDataToInflux({ data, symbol });
 };
 
